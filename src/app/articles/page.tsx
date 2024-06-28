705aff86f24d5ae1763b431cd206c424dac97c39
Md-Abdullah-321 <abdullah.dev.it@gmail.com>
2024-06-28 14:58:38 +0600

    Created basic project, added required routes and pages

---
+++ src/app/articles/page.tsx
@@ -0,0 +1,3 @@
+export default function Articles() {
+  return <h1>Article Listing Page.</h1>;
+}
---
+++ src/app/single-article/page.tsx
@@ -0,0 +1,3 @@
+export default function SingleArticle() {
+  return <h1>Single Article Page.</h1>;
+}
